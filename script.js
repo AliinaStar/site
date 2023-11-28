d6dc14e6ca4da8e6c082b2f200f515094f215f48
AliinaStar <128737579+AliinaStar@users.noreply.github.com>
2023-11-28 22:22:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const burgerMenu = document.getElementById('burger-menu');
+const headerNav = document.querySelector('.menu1');
+burgerMenu.addEventListener('click', function() {
+    headerNav.classList.toggle('active');
+});
+
+
